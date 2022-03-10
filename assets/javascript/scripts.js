e4c034848993d42c16d0388573dc6f1172bc393c
yumikawano <camila.domingos@al.infnet.edu.br>
2022-03-09 22:05:38 -0300

    criando html dinamico

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,83 +1,59 @@
-const cartSidebarEl = document.querySelector('.cart-sidebar')
-function openSidebar () {
-  cartSidebarEl.classList.add('cart-sidebar-open')
+const cartSidebarEl = document.querySelector(".cart-sidebar");
+function openSidebar() {
+  cartSidebarEl.classList.add("cart-sidebar-open");
 }
-function closeSidebar () {
-  cartSidebarEl.classList.remove('cart-sidebar-open')
-}
-const btnCartEl = document.getElementById('btn-cart')
-btnCartEl.addEventListener('click', openSidebar)
-const btnCloseCartEl = document.querySelector('#btn-close-cart')
-btnCloseCartEl.addEventListener('click', closeSidebar)
 
-const fetchProducts = () => {
-  const groupsRootEl = document.querySelector('#groups-root')
-  fetch('/servicos.json')
-    .then(res => res.json())
-    .then(data => {
-      groupsRootEl.innerHTML = ''
-      data.groups.forEach((group) => {
-        const groupSectionEl = getSectionElement(group)
-        groupsRootEl.appendChild(groupSectionEl)
-      })
-    })
-    .catch(() => {
-      groupsRootEl.innerHTML = '<p class="error-alert">Falha ao buscar produtos. Por favor, tente novamente.</p>'
-    })
+function closeSidebar() {
+  cartSidebarEl.classList.remove("cart-sidebar-open");
 }
-const getSectionElement = (group) => {
-  const sectionEl = document.createElement('section')
-  const sectionTitleEl = document.createElement('h2')
-  sectionTitleEl.textContent = group.name
-  sectionEl.appendChild(sectionTitleEl)
-  const productsGridEl = document.createElement('div')
-  productsGridEl.classList.add('products-grid')
-  sectionEl.appendChild(productsGridEl)
-  group.products.forEach((product) => {
-    const cardWrapEl = document.createElement('article')
-    cardWrapEl.classList.add('card')
-    cardWrapEl.innerHTML = `
-      <img src="${product.image}" alt="${product.name}" width="316" height="193" />
-      <div class="card-content">
-        <h3>${product.name}</h3>
-        <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</p>
-        ${product.description ? `<p>${product.description}</p>` : ''}
-        <button class="btn btn-contratar">Contratar</button>
-      </div>
-    `
-    const btnAddCartEl = cardWrapEl.querySelector('button')
-    btnAddCartEl.addEventListener('click', () => {
-      addToCart(product)
-    })
-    productsGridEl.appendChild(cardWrapEl)
-  })
-  return sectionEl
-}
-fetchProducts()
 
-const productsCart = []
-const addToCart = newProduct => {
-  const productIndex = productsCart.findIndex(
-    item => item.id === newProduct.id
-  )
-  if (productIndex === -1) {
-    productsCart.push({
-      ...newProduct,
-      qty: 1
+const btnCartEl = document.getElementById("btn-cart");
+btnCartEl.addEventListener("click", openSidebar);
+const btnCloseCartEl = document.querySelector("#btn-close-cart");
+btnCloseCartEl.addEventListener("click", closeSidebar);
+
+/* CHAMEI O JSON */
+
+const fetchServices = () => {
+  const servicesRoot = document.querySelector("#services");
+  const responseJson = fetch("/services.json");
+  console.log(responseJson);
+  responseJson
+    .then((data) => {
+      const dataJson = data.json();
+      console.log(data, "acabou os dados");
+      dataJson.then((object) => {
+        console.log(object, "acabou");
+        servicesRoot.innerHTML = ''
+        object.services.forEach((service) => {
+          const functionHtml = html(service);
+          servicesRoot.appendChild(functionHtml);
+          console.log(service.image);
+        });
+      });
     })
-  } else {
-    productsCart[productIndex].qty++
-  }
-  handleCartUpdate()
+    .catch((e) => console.log(e));
+};
+
+/* CRIEI O HTML */
+
+const html = (service) => {
+  const sectionEl = document.createElement("section");
+  sectionEl.classList.add("services");
+  sectionEl.innerHTML = `<img class="item12" src="${service.image}"/>
+    <h3 class="name" style="  margin-bottom: 8px;
+    margin-left: 15px;
+    margin-right: 15px;">${service.name}</h3>
+    <p class="description">${service.description ? `<p class="description" style="  margin-bottom: 8px;
+    margin-left: 15px;
+    margin-right: 15px;">${service.description}</p>
+    <p class="price" style="  margin-bottom: 8px;
+    margin-left: 15px;
+    margin-right: 15px;">R$ ${service.price.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</p>` : ''}
+    <button class="btn btn-contratar" style="  margin-bottom: 8px;
+    margin-left: 15px;
+    margin-right: 15px;">Contratar</button>`;
+
+  return sectionEl;
 }
-const handleCartUpdate = () => {
-  if (productsCart.length > 0) {
-    const cartBadgeEl = document.querySelector('.btn-cart-badge')
-    cartBadgeEl.classList.add('btn-cart-badge-show')
-    let total = 0
-    productsCart.forEach(product => {
-      total = total + product.qty
-    })
-    cartBadgeEl.textContent = total
-  }
-}
+fetchServices()
---
+++ assets/javascript/teste.js
@@ -0,0 +1,11 @@
+const teste = ['mochila, estojo, lapis']
+for(let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+teste.forEach(teste1(nome));
+
+function teste1(nome) {
+    
+}
+
